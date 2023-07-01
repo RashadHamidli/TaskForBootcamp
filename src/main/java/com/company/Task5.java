162dfeec4007b5af904cb1e599c6a926d3dba5ac
Rashad Hamid <mr_rashad@email.com>
2023-07-01 09:33:58 +0400

    add

--- src/main/java/com/company/Task5.java
+++ src/main/java/com/company/Task5.java
@@ -4,13 +4,19 @@ import java.util.Scanner;
 
 public class Task5 {
     public static void main(String[] args) {
-        for (int i = 1; i < 100; i++) {
+
+    }
+    public static void printEvenNumbers(){
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("ededi daxil edin: ");
+        int num = scanner.nextInt();
+        for (int i = 1; i < num; i++) {
             if (i % 2 == 0)
                 System.out.print(i + ", ");
         }
     }
 
-    public static void foo() {
+    public static void printA_Z() {
         for (char c = 65; c <= 90; c++) {
             System.out.println(c);
         }
