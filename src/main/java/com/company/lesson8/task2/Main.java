6a0df9f58b114463d93cc568ce7d83b5bac880e9
Rashad Hamid <mr_rashad@email.com>
2023-07-14 22:10:22 +0400

    add

--- src/main/java/com/company/lesson8/task2/Main.java
+++ src/main/java/com/company/lesson8/task2/Main.java
@@ -2,7 +2,9 @@ package com.company.lesson8.task2;
 
 public class Main {
     public static void main(String[] args) {
-        Kvadrat kvadrat=new Kvadrat(3, 5);
+        Kvadrat kvadrat=new Kvadrat(5, 5);
         kvadrat.fiqurunSahesi();
+        Duzbucaqli d=new Duzbucaqli(3, 5);
+        d.fiqurunSahesi();
     }
 }
---
+++ src/main/java/com/company/lesson8/task3/Main.java
@@ -0,0 +1,12 @@
+package com.company.lesson8.task3;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("Rashad");
+        person.setNationality("Azerbaijan");
+        person.setAge(29);
+        System.out.println("Name: " + person.getName() + "\nNationality: " + person.getNationality() + "\nAge: " + person.getAge());
+
+    }
+}
