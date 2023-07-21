bb45d0f4a878d7e921b3bff686369d0ca7955bec
Rashad Hamid <mr_rashad@email.com>
2023-07-21 16:34:30 +0400

    add

--- src/main/java/com/object/lambda/Lambda.java
+++ src/main/java/com/object/lambda/Lambda.java
@@ -1,13 +1,20 @@
 package com.object.lambda;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Lambda {
     public static void main(String[] args) {
-        List<String> words = Arrays.asList("soz1", "soz2", "soz3");
-        for (String word : words) {
-            System.out.println(word);
-        }
+//        List<String> words = Arrays.asList("soz1", "soz2", "soz3");
+//        words.forEach(System.out::println);
+
+        List<Integer> num = new ArrayList<>();
+        num.add(14);
+        num.add(36);
+        num.add(21);
+        num.forEach(s-> System.out.println(s));
+        num.forEach(System.out::println);
+
     }
 }
