66d6986e83e1820417057475b244bc2d5731b673
Rashad Hamid <mr_rashad@email.com>
2023-07-21 14:44:59 +0400

    added

--- src/main/java/com/object/lambda/Lambda.java
+++ src/main/java/com/object/lambda/Lambda.java
@@ -1,9 +1,13 @@
 package com.object.lambda;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Lambda {
     public static void main(String[] args) {
-        String a="Salam";
-        Runnable runnable = () -> a.toUpperCase();
-        System.out.println(runnable);
+        List<String> words = Arrays.asList("soz1", "soz2", "soz3");
+        for (String word : words) {
+            System.out.println(word);
+        }
     }
 }
