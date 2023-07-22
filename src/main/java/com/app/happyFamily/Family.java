fec50e6093fe3f520919b56fd72962f26969d538
Rashad Hamid <mr_rashad@email.com>
2023-07-22 14:59:39 +0400

    add

--- src/main/java/com/app/happyFamily/Family.java
+++ src/main/java/com/app/happyFamily/Family.java
@@ -6,22 +6,34 @@ public class Family {
     public Human father;
     public Human mother;
     public Human[] child;
-    public void allFamily(Father father, Mother mother, Child child){
-        this.father=father;
-        this.mother=mother;
-        this.child= new Child[]{child};
+
+    public void addChild() {
+        
     }
+
+    public void countFamily(Father father, Mother mother, Child child) {
+        this.father = father;
+        this.mother = mother;
+        this.child = new Child[]{child};
+    }
+
+    public boolean deleteChild() {
+        return true;
+    }
+
 
     public Child[] addChild(Child[] child) {
         this.child = child;
         return child;
     }
-    public Father addFather(Father father){
-        this.father=father;
+
+    public Father addFather(Father father) {
+        this.father = father;
         return father;
     }
-    public Mother addMother(Mother mother){
-        this.mother=mother;
+
+    public Mother addMother(Mother mother) {
+        this.mother = mother;
         return mother;
     }
 
--- src/main/java/com/app/happyFamily/Mother.java
+++ src/main/java/com/app/happyFamily/Mother.java
@@ -1,7 +1,6 @@
 package com.app.happyFamily;
 
 public class Mother extends Human{
-
     public Mother(String name, String surname, int year, int iq) {
         super(name, surname, year, iq);
     }
