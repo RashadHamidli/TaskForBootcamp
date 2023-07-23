7f634dc80c8a9722c127c4bde006f573466e8a6a
Rashad Hamid <mr_rashad@email.com>
2023-07-23 17:54:42 +0400

    add

---
+++ src/main/java/com/app/pet/Man.java
@@ -0,0 +1,11 @@
+package com.app.pet;
+
+public class Man extends Human {
+    public Man(String name, String surname, int age) {
+        super(name, surname, age);
+    }
+
+    public String repairCar() {
+    return "man is repairing a car";
+    }
+}
