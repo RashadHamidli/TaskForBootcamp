2fec38faaa4d730880239f974398660079d3f1fc
Rashad Hamid <mr_rashad@email.com>
2023-07-24 14:26:34 +0400

    add

--- src/main/java/com/object/lambda/Main.java
+++ src/main/java/com/object/lambda/Main.java
@@ -3,6 +3,7 @@ package com.object.lambda;
 public class Main {
     public static void main(String[] args) {
         Calc calc = () -> sumTwoInt(5, 3);
+        System.out.println(calc);
     }
 
     public static int sumTwoInt(int a, int b) {
