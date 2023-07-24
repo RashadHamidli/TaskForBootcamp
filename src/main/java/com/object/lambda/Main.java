8c054f12a70e8e64bd86d3e645a3a32df130e75b
Rashad Hamid <mr_rashad@email.com>
2023-07-24 15:47:50 +0400

    add

--- src/main/java/com/object/lambda/Main.java
+++ src/main/java/com/object/lambda/Main.java
@@ -2,8 +2,7 @@ package com.object.lambda;
 
 public class Main {
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        audibleMehtod(dog);
+
     }
 
     public static void audibleMehtod(Audible audible) {
