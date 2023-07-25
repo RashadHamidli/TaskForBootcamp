8f1d0a7bae03d59c03ec10b21f2abc62bd615395
Rashad Hamid <mr_rashad@email.com>
2023-07-25 15:14:22 +0400

    add

--- src/main/java/com/company/lesson16/task3/Max.java
+++ src/main/java/com/company/lesson16/task3/Max.java
@@ -6,6 +6,8 @@ public class Max {
         System.out.println(max);
         Double maxD = max(3.5, 4.5, 5.6);
         System.out.println(maxD);
+        String s = maxString("a", "v", "r");
+        System.out.println(s);
     }
 
     public static <T extends Number> T max(T x, T y, T z) {
@@ -15,6 +17,16 @@ public class Max {
         }
         if (z.intValue() > y.intValue()) {
             max = z;
+        }
+        return max;
+    }
+    public static <E extends String> E maxString(E x, E y, E z){
+        E max=x;
+        if(y.compareTo(x)>0){
+            max=y;
+        }
+        if(z.compareTo(y)>0){
+            max=z;
         }
         return max;
     }
