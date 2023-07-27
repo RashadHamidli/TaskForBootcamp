5712195631616b7082598f06b5a15f791dfb4f89
Rashad Hamid <mr_rashad@email.com>
2023-07-27 19:16:37 +0400

    add

--- src/main/java/com/company/lesson16/task5/Main.java
+++ src/main/java/com/company/lesson16/task5/Main.java
@@ -1,4 +1,21 @@
 package com.company.lesson16.task5;
 
 public class Main {
+    public static void main(String[] args) {
+        Integer i = 5;
+
+        byte b = i.byteValue();
+        int i1 = i.intValue();
+        double v = i.doubleValue();
+        long l = i.longValue();
+        short s = i.shortValue();
+        float v1 = i.floatValue();
+        System.out.println(b);
+        System.out.println(i1);
+        System.out.println(v);
+        System.out.println(l);
+        System.out.println(s);
+        System.out.println(v1);
+
+    }
 }
