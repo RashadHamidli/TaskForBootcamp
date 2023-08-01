b79393a756ffdf81d8c5612472d59c6741b4e84f
Rashad Hamid <mr_rashad@email.com>
2023-08-01 19:27:48 +0400

    add

--- src/main/java/com/company/lesson16/task5/Foo.java
+++ src/main/java/com/company/lesson16/task5/Foo.java
@@ -1,4 +1,11 @@
 package com.company.lesson16.task5;
 
-public class Foo<T extends Exception> {
+public class Foo {
+    public static void main(String[] args) {
+        String s="Rashad";
+        String ss="Rashad";
+        if(s==ss){
+            System.out.println(true);
+        }
+    }
 }
