d7b63ca19e92a3f91228e58d498b1356ad6f2b7c
Rashad Hamid <mr_rashad@email.com>
2023-08-02 15:25:09 +0400

    add

--- src/main/java/com/company/lesson16/task5/Foo.java
+++ src/main/java/com/company/lesson16/task5/Foo.java
@@ -2,10 +2,6 @@ package com.company.lesson16.task5;
 
 public class Foo {
     public static void main(String[] args) {
-        String s="Rashad";
-        String ss="Rashad";
-        if(s==ss){
-            System.out.println(true);
-        }
+
     }
 }
