785ab646cd2b7f5bdd47fac83a1e6063886661bd
Rashad Hamid <mr_rashad@email.com>
2023-08-02 20:08:00 +0400

    add

--- src/main/java/com/company/lesson18/task2/ArrayListT.java
+++ src/main/java/com/company/lesson18/task2/ArrayListT.java
@@ -4,33 +4,6 @@ import java.util.List;
 
 public class ArrayListT {
     public static void main(String[] args) {
-        List<String> list = new java.util.ArrayList<>(10);
-        list.add("a");
-        list.add("b");
-        list.add("d");
-        list.add(2, "c");
-        list.add(4, "e");
-        list.add("c");
-        list.add(9,"J");
 
-        list.forEach(System.out::println);
-        Object[] array = list.toArray();
-        for (Object o : array) {
-            System.out.println(o);
-        }
-        int i = list.lastIndexOf("c");
-        System.out.println(i);
-//        long count = list.stream().count();
-//        System.out.println("count: " + count);
-//        list.remove(3);
-//        int size = list.size();
-//        System.out.println("size: " + size);
-//
-//        boolean b = list.contains("a");
-//        System.out.println(b);
-//        String s = list.get(3);
-//        System.out.println(s);
-//        Collections.sort(list);
-//        list.forEach(System.out::println);
     }
 }
--- src/main/java/com/company/lesson18/task2/LinkedListT.java
+++ src/main/java/com/company/lesson18/task2/LinkedListT.java
@@ -7,28 +7,7 @@ import java.util.List;
 
 public class LinkedListT {
     public static void main(String[] args) {
-        LinkedList<String> list = new LinkedList<>();
-        list.addFirst("a");
-        list.addLast("z");
-        list.add("b");
-        list.add("c");
-        list.add(3, "d");
-        list.add(4, "e");
 
-
-        list.forEach(System.out::println);
-        long count = list.stream().count();
-        System.out.println(count);
-        int size = list.size();
-        System.out.println(size);
-//        list.remove(2);
-//        list.remove("c");
-//        list.forEach(System.out::println);
-//        Collections.sort(list);
-//        list.forEach(System.out::println);
-//        list.removeFirst();
-//        list.removeLast();
-//        list.forEach(System.out::println);
 
     }
 }
