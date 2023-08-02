635e6227eb66d26f096629f8133b0c2b5a77f777
Rashad Hamid <mr_rashad@email.com>
2023-08-02 20:21:14 +0400

    add

--- src/main/java/com/company/lesson18/task2/ArrayListT.java
+++ src/main/java/com/company/lesson18/task2/ArrayListT.java
@@ -1,9 +1,41 @@
 package com.company.lesson18.task2;
 
-import java.util.List;
+import java.util.*;
 
 public class ArrayListT {
     public static void main(String[] args) {
 
+        ArrayList<String> list = new ArrayList<>();
+        list.add("red");
+        list.add("blue");
+        list.add("green");
+        list.add("black");
+        list.add("yellow");
+        list.forEach(System.out::println);
+        System.out.println("------------------");
+
+        Object clone = list.clone();
+        System.out.println(clone);
+        System.out.println("------------------");
+
+        list.remove(3);
+        list.forEach(System.out::println);
+        System.out.println("------------------");
+
+        String s = list.get(1);
+        System.out.println(s);
+        System.out.println("------------------");
+
+        Collections.sort(list);
+        list.forEach(System.out::println);
+        System.out.println("------------------");
+
+        Object[] array = list.toArray();
+        for(Object obj: array){
+            System.out.println(obj);
+        }
+        System.out.println("------------------");
+
     }
 }
+
