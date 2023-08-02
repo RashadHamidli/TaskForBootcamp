c0db946a272ae3f382d9a8f245b372ccb249f610
Rashad Hamid <mr_rashad@email.com>
2023-08-02 20:29:00 +0400

    add

--- src/main/java/com/company/lesson18/task2/LinkedListT.java
+++ src/main/java/com/company/lesson18/task2/LinkedListT.java
@@ -1,13 +1,29 @@
 package com.company.lesson18.task2;
 
-import java.util.Collection;
-import java.util.Collections;
+
 import java.util.LinkedList;
 import java.util.List;
 
 public class LinkedListT {
     public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        list.addLast("red");
 
+        list.add(1, "black");
+
+        list.addFirst("white");
+        list.addLast("grey");
+
+        String first = list.getFirst();
+        System.out.println(first);
+        String last = list.getLast();
+        System.out.println(last);
+
+        list.forEach(System.out::println);
+        list.remove("red");
+        list.removeFirst();
+        list.removeLast();
+        list.forEach(System.out::println);
 
     }
 }
