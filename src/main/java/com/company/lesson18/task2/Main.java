dc23019cd1b717c72963ac38deb2163011baccbc
Rashad Hamid <mr_rashad@email.com>
2023-08-02 18:42:41 +0400

    add

--- src/main/java/com/company/lesson18/task2/Main.java
+++ src/main/java/com/company/lesson18/task2/Main.java
@@ -5,11 +5,27 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> list=new ArrayList<>();
-        list.add("baki");
-        list.forEach(a-> System.out.println("ilk cap "+a));
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("d");
+        list.add(2, "c");
+        list.add(4, "e");
+
+        list.forEach(System.out::println);
+        long l = list.stream().count();
+        System.out.println(l);
+
         list.remove(2);
-        list.forEach(a-> System.out.println("ikinci cap "+a));
+        System.out.println("-------------------");
+
+        list.forEach(System.out::println);
+
+        long count = list.stream().count();
+        System.out.println(count);
+
+        list.clear();
+        list.forEach(a -> System.out.println("cleardan sonra massiv"+a));
 
 
     }
