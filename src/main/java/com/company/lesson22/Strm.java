b8306d06316d878b969c298e7651af84d51bd497
Rashad Hamid <mr_rashad@email.com>
2023-08-13 21:07:58 +0400

    add

--- src/main/java/com/company/lesson22/Strm.java
+++ src/main/java/com/company/lesson22/Strm.java
@@ -9,13 +9,19 @@ import java.util.stream.Stream;
 public class Strm {
     public static void main(String[] args) {
 
-        List<String> list = Arrays.asList("Baku", "Ganca", "Quba", "Qakh", "Lacin");
-        List<String> l = list.stream().map(s -> s.toUpperCase()).toList();
-        Stream<String> s = list.stream().map(String::toUpperCase);
-        s.forEach(System.out::println);
-        s.close();
-        Stream<String> a = list.stream().map(String::toLowerCase);
-        a.forEach(System.out::println);
 
+//        List<String> list = Arrays.asList("Baku", "Ganca", "Quba", "Qakh", "Lacin");
+//        List<String> l = list.stream().map(s -> s.toUpperCase()).toList();
+//        Stream<String> s = list.stream().map(String::toUpperCase);
+//        s.forEach(System.out::println);
+//        s.close();
+//        Stream<String> a = list.stream().map(String::toLowerCase);
+//        a.forEach(System.out::println);
+
+    }
+
+    public static void lowerCase(String s){
+        String upperCase = s.toUpperCase();
+        System.out.println(upperCase);
     }
 }
