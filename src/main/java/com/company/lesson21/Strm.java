ca79584af7a3b8d5baff0e255ca306782b8c8f5f
Rashad Hamid <mr_rashad@email.com>
2023-08-13 13:38:40 +0400

    add

--- src/main/java/com/company/lesson21/Strm.java
+++ src/main/java/com/company/lesson21/Strm.java
@@ -7,11 +7,11 @@ import java.util.stream.Stream;
 public class Strm {
     public static void main(String[] args) {
 
-        List<Integer> list = List.of(1, 3, 5, 7, 9, 2, 4, 6, 8);
-        Stream<Integer> l = list.stream()
+        List<Integer> list = List.of(1, 3, 5, 4, 8, 6, 7, 9, 2, 4, 6, 8, 1, 2, 3);
+        List<Integer> l = list.stream()
+                .distinct()
                 .filter(i -> i % 2 == 0)
-                .filter(i -> i > 5)
-                .filter(i -> i < 7);
+                .toList();
         l.forEach(System.out::println);
 
     }
