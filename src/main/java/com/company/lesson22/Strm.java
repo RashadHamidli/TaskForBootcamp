f5fa64dda3f8b72c3669e370fb66eb7f81f70da1
Rashad Hamid <mr_rashad@email.com>
2023-08-13 19:15:07 +0400

    add

--- src/main/java/com/company/lesson22/Strm.java
+++ src/main/java/com/company/lesson22/Strm.java
@@ -1,6 +1,7 @@
 package com.company.lesson22;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
@@ -8,11 +9,13 @@ import java.util.stream.Stream;
 public class Strm {
     public static void main(String[] args) {
 
-        List<Integer> list = List.of(1, 3, 5, 4, 8, 6, 7, 9, 2, 4, 6, 8, 1, 2, 3);
-        Integer reduce = list.stream()
-                .reduce(0, (a, b) -> a + b);
-        System.out.println(reduce);
-       
+        List<String> list = Arrays.asList("Baku", "Ganca", "Quba", "Qakh", "Lacin");
+        List<String> l = list.stream().map(s -> s.toUpperCase()).toList();
+        Stream<String> s = list.stream().map(String::toUpperCase);
+        s.forEach(System.out::println);
+        s.close();
+        Stream<String> a = list.stream().map(String::toLowerCase);
+        a.forEach(System.out::println);
 
     }
 }
