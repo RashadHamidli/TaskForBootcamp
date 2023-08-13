ab2779eb0914a04c76489260d0a3903b8120cf2f
Rashad Hamid <mr_rashad@email.com>
2023-08-13 13:26:45 +0400

    add

--- src/main/java/com/company/lesson20/task1.java
+++ src/main/java/com/company/lesson20/task1.java
@@ -1,4 +1,4 @@
-package com.company.lesson;
+package com.company.lesson20;
 
 import java.util.ArrayList;
 import java.util.List;
---
+++ src/main/java/com/company/lesson21/Strm.java
@@ -0,0 +1,9 @@
+package com.company.lesson21;
+
+public class Strm {
+    public static void main(String[] args) {
+
+
+
+    }
+}
