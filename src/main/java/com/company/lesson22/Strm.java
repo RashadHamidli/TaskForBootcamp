6479f1ed525a8dede646d16521225ce8bee7df2f
Rashad Hamid <mr_rashad@email.com>
2023-08-14 14:41:28 +0400

    add

--- src/main/java/com/company/lesson22/Strm.java
+++ src/main/java/com/company/lesson22/Strm.java
@@ -1,23 +1,20 @@
 package com.company.lesson22;
 
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Strm {
     public static void main(String[] args) {
 
-        Stream<Integer> i = Stream.of(1, 2, 3, 4, 5)
-                .map(m -> m * 2)
-                .distinct()
-                .filter(f -> f.equals(2));
-        i.forEach(System.out::println);
-        Consumer consumer;
-        Predicate predicate;
-        Function function;
-        Supplier supplier;
+        String nullableValue = "deyer var";
+        Optional<String> optionalNullableValue = Optional.ofNullable(nullableValue);
+        String s = optionalNullableValue.orElse("deyer yoxdur");
+        System.out.println(s);
 
 
     }
