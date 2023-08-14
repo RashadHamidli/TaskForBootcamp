9634ee3398d87f80b4acec16bb16f44d1040c1dc
Rashad Hamid <mr_rashad@email.com>
2023-08-14 09:33:47 +0400

    add

--- src/main/java/com/company/lesson22/Strm.java
+++ src/main/java/com/company/lesson22/Strm.java
@@ -1,27 +1,25 @@
 package com.company.lesson22;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class Strm {
     public static void main(String[] args) {
 
+        Stream<Integer> i = Stream.of(1, 2, 3, 4, 5)
+                .map(m -> m * 2)
+                .distinct()
+                .filter(f -> f.equals(2));
+        i.forEach(System.out::println);
+        Consumer consumer;
+        Predicate predicate;
+        Function function;
+        Supplier supplier;
 
-//        List<String> list = Arrays.asList("Baku", "Ganca", "Quba", "Qakh", "Lacin");
-//        List<String> l = list.stream().map(s -> s.toUpperCase()).toList();
-//        Stream<String> s = list.stream().map(String::toUpperCase);
-//        s.forEach(System.out::println);
-//        s.close();
-//        Stream<String> a = list.stream().map(String::toLowerCase);
-//        a.forEach(System.out::println);
 
     }
 
-    public static void lowerCase(String s){
-        String upperCase = s.toUpperCase();
-        System.out.println(upperCase);
-    }
 }
