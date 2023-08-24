7c288df7cd00b853a6308de5c634c46330f568a0
Rashad Hamid <mr_rashad@email.com>
2023-08-24 09:35:35 +0400

    add

--- src/main/java/com/company/lesson23/Main.java
+++ src/main/java/com/company/lesson23/Main.java
@@ -1,25 +1,18 @@
 package com.company.lesson23;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(2, 5, 6, 1, 8, 6, 4, 9, 2);
-        Optional<Integer> max = list.stream().min((o1, o2) -> o1);
-        System.out.println(max);
-
+        averageStream();
     }
 
     public static void averageStream() {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        Integer reduce = list.stream().reduce(0, (a, b) -> a + b);
-        int size = list.size();
-        int avr = reduce / size;
-        System.out.println(avr);
+        OptionalDouble average = list.stream().mapToInt(Integer::intValue)
+                .average();
+        System.out.println(average);
     }
 
     public static void toLowerUpperCaseInStream() {
@@ -42,5 +35,23 @@ public class Main {
     public static void removeDublicatElemByStream(){
         List<Integer> list = Arrays.asList(1, 2, 5, 3, 4, 2, 6, 9, 5, 3, 7, 8, 1, 4, 6);
         list.stream().distinct().forEach(System.out::println);
+    }
+    public static void minAndMaxSecondElemByStream(){
+        List<Integer> list = Arrays.asList(2, 5, 6, 1, 8, 6, 4, 9, 2);
+        Integer min = list.stream()
+                .distinct()
+                .sorted()
+                .skip(1)
+                .findFirst()
+                .orElse(null);
+        System.out.println("min second element: "+min);
+
+        Integer max = list.stream()
+                .distinct()
+                .sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst()
+                .orElse(null);
+        System.out.println("max second element: "+max);
     }
 }
