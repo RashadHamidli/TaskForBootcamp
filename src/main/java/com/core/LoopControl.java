6b38620e1e287c44e643475bb3faa7ea0951e5ac
mr_rashad <mr_rashad@email.com>
2023-11-28 17:19:44 +0400

    add

--- src/main/java/com/core/LoopControl.java
+++ src/main/java/com/core/LoopControl.java
@@ -1,9 +1,15 @@
 package com.core;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class LoopControl {
     public static void main(String[] args) {
         Student[] students = {new Student(1L, "aa", "aa"),
                 new Student(2L, "bb", "bb")};
+
+        List<String> list = Arrays.stream(students).map(Student::toString).toList();
+        System.out.println(list);
 
         for (Student s : students) {
             System.out.println(s);
