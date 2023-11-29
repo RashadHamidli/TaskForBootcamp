8beda719a0bdffd02ba578fc491643b70833cbfc
mr_rashad <mr_rashad@email.com>
2023-11-29 19:01:31 +0400

    ADD

--- src/main/java/com/OOP/poly/Main.java
+++ src/main/java/com/OOP/poly/Main.java
@@ -2,15 +2,27 @@ package com.OOP.poly;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        LocalDateTime localDateTime = LocalDateTime.now();
-        System.out.println(localDateTime);
 
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("E, dd.MM.yyyy HH:mm:ss");
-        String format = dateTimeFormatter.format(localDateTime);
-        System.out.println(format);
-
+        ArrayList<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("c");
+        list.add("d");
+        list.add("b");
+        Collections.sort(list);
+        for (String l : list) {
+            System.out.println(l);
+        }
+        System.out.println("---------------------");
+        list.forEach(System.out::println);
+        System.out.println("---------------------");
+        Stream<String> sortedList = list.stream().sorted();
+        sortedList.forEach(System.out::println);
     }
 }
