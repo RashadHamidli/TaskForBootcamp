8d0690510821d0883811d618b6288337fcd045f0
mr_rashad <mr_rashad@email.com>
2023-11-29 17:59:20 +0400

    add

--- src/main/java/com/OOP/poly/Main.java
+++ src/main/java/com/OOP/poly/Main.java
@@ -1,7 +1,16 @@
 package com.OOP.poly;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("E, dd.MM.yyyy HH:mm:ss");
+        String format = dateTimeFormatter.format(localDateTime);
+        System.out.println(format);
 
     }
 }
