5ffc53c6bd74c088c4b050c428d95f06dff6de11
mr_rashad <mr_rashad@email.com>
2023-12-02 12:06:56 +0400

    add

--- src/main/java/com/OOP/poly/Salary.java
+++ src/main/java/com/OOP/poly/Salary.java
@@ -1,32 +1,12 @@
 package com.OOP.poly;
 
 
+import java.io.IOException;
+
 public class Salary extends Employee {
-    private double salary; // Annual salary
-
-    public Salary(String name, String address, int number, double salary) {
-        super(name, address, number);
-        setSalary(salary);
-    }
-
-    public void mailCheck() {
-        System.out.println("Within mailCheck of Salary class ");
-        System.out.println("Mailing check to " + getName()
-                + " with salary " + salary);
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double newSalary) {
-        if(newSalary >= 0.0) {
-            salary = newSalary;
-        }
-    }
-
-    public double computePay() {
-        System.out.println("Computing salary pay for " + getName());
-        return salary/52;
+    @Override
+    public void foo() {
+        super.foo();
+        System.out.println("foo in Salary");
     }
 }
