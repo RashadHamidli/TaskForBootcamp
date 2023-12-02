7cc3148b0b4acb61c5799a71de3d55b0663ee0e8
mr_rashad <mr_rashad@email.com>
2023-12-02 20:13:42 +0400

    add

--- src/main/java/com/exception/Test.java
+++ src/main/java/com/exception/Test.java
@@ -1,17 +1,24 @@
 package com.exception;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("proqram basladi");
         try {
             BankAccount b = new BankAccount(1);
             b.deposit(500);
             System.out.println("balance: " + b.getBalance());
-            b.transfer(289);
-            System.out.println("balance: " + b.getBalance());
-            b.transfer(355);
-            System.out.println("balance: " + b.getBalance());
+            while (true) {
+                System.out.print("transfer meblegini daxil edin: ");
+                b.transfer(scanner.nextDouble());
+                System.out.println("balance: " + b.getBalance());
+            }
         } catch (MyExceptionHandler e) {
             System.out.println("hesabinizda " + e.getSum() + "manat mebleg catmir");
+            e.printStackTrace();
         }
+        System.out.println("proqram davam edir");
     }
 }
