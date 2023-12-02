f7ba2c4457b8e98a16b415f21d9502577ee388db
mr_rashad <mr_rashad@email.com>
2023-12-02 18:45:16 +0400

    add

--- src/main/java/com/io/oop/CopyFile.java
+++ src/main/java/com/io/oop/CopyFile.java
@@ -4,15 +4,44 @@ import java.io.*;
 
 public class CopyFile {
     public static void main(String[] args) {
-        try (InputStreamReader inputStreamReader = new InputStreamReader(System.in)) {
-            System.out.println("Enter characters, 'q' to quit.");
-            char c;
-            do {
-                c = (char) inputStreamReader.read();
-            } while (c != 'q');
+
+
+        byte[] arr = {1, 5, 91, 6, 47, 8, 26, 3, 4, 66, 5, 9, 87, 4, 2, 6, 7, 13, 6, 4, 45, 23, 1, 8, 45, 81, 5, 8, 14};
+
+
+        File file = new File("a.txt");
+        System.out.println("file.mkdir() - " + file.mkdir());
+        System.out.println("file.mkdirs() - " + file.mkdirs());
+
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream("test.txt")) {
+            for (byte b : arr) {
+                fileOutputStream.write(b);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        try (FileInputStream fileInputStream = new FileInputStream("test.txt")) {
+            int size = fileInputStream.available();
+
+            for (int i = 0; i < size; i++) {
+                System.out.print((char) fileInputStream.read() + "  ");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+//        try (InputStreamReader inputStreamReader = new InputStreamReader(System.in)) {
+//            System.out.println("Enter characters, 'q' to quit.");
+//            char c;
+//            do {
+//                c = (char) inputStreamReader.read();
+//            } while (c != 'q');
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
 //        try (InputStreamReader inputStreamReader = new InputStreamReader(System.in)) {
 //            System.out.println("Enter characters, 'q' to quit.");
