c0b2b10baae537939c68cd6652ca220c74eccd2c
mr_rashad <mr_rashad@email.com>
2023-12-08 09:44:26 +0400

    add

--- src/main/java/com/feature/Main.java
+++ src/main/java/com/feature/Main.java
@@ -2,20 +2,14 @@ package com.feature;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        var list = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
-        list.stream().forEach(System.out::println);
-        System.out.println("---------------------------------------");
-        list.parallelStream().forEach(System.out::println);
-
         var a = Arrays.asList(1, 2, 3);
         var b = Arrays.asList(4, 5, 6);
         var c = Arrays.asList(7, 8, 9);
 
-        var l = List.of(a, b, c);
-        l.stream().forEach(System.out::print);
-//        l.stream().flatMap(i -> Arrays.stream(i)).forEach(System.out::println);
+        Stream.of(a, b, c).flatMap(List::stream).forEach(s -> System.out.print(s + ", "));
     }
 }
