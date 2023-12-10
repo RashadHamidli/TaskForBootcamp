188dc7a778b1f381736346aef1a7cde21932df7a
mr_rashad <mr_rashad@email.com>
2023-12-10 10:06:06 +0400

    add

--- src/main/java/com/company/lesson14/task3/Bicycle.java
+++
@@ -1,8 +0,0 @@
-package com.company.lesson14.task3;
-
-public class Bicycle extends Vehicle{
-    @Override
-    public double speedUp() {
-        return super.speedUp()+15;
-    }
-}
--- src/main/java/com/company/lesson14/task3/Car.java
+++
@@ -1,8 +0,0 @@
-package com.company.lesson14.task3;
-
-public class Car extends Vehicle {
-    @Override
-    public double speedUp() {
-        return super.speedUp()+15;
-    }
-}
