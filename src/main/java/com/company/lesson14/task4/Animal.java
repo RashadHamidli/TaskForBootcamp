85f0a020856b2907b79c5688de77ee5f370191ed
mr_rashad <mr_rashad@email.com>
2023-12-10 10:06:23 +0400

    add

--- src/main/java/com/company/lesson14/task4/Animal.java
+++
@@ -1,5 +0,0 @@
-package com.company.lesson14.task4;
-
-public abstract class Animal {
-    public abstract String makeSound();
-}
--- src/main/java/com/company/lesson14/task4/Snake.java
+++
@@ -1,8 +0,0 @@
-package com.company.lesson14.task4;
-
-public class Snake extends Animal{
-    @Override
-    public String makeSound() {
-        return getClass().getSimpleName()+" sound";
-    }
-}
