780963902e80b1e14463ea3bece39d9d262c57bc
RashadHamidli <mr_rashad@email.com>
2023-12-28 11:30:14 +0400

    add

--- src/main/java/az/company/config/Configuration.java
+++ src/main/java/az/company/config/Configuration.java
@@ -1,6 +1,8 @@
 package az.company.config;
 
 import az.company.entities.Message;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import lombok.Singular;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,18 +11,9 @@ import org.springframework.context.annotation.DependsOn;
 @org.springframework.context.annotation.Configuration
 @ComponentScan
 public class Configuration {
-    @Bean(name = "message1")
-    public Message message() {
-        Message message = new Message();
-        message.setName("Linkedin message");
-        return message;
-    }
-    @Bean(name = "message2")
-    @DependsOn
-    public Message message2() {
-        Message message = new Message();
-        message.setName("Facebook message");
-        return message;
+    @Bean
+   public EntityManagerFactory entityManagerFactory(){
+        return Persistence.createEntityManagerFactory()
     }
 
 
