876c49add570564081550fff9af389f04441ee5d
RashadHamidli <mr_rashad@email.com>
2024-01-04 19:24:40 +0400

    add

--- src/main/java/com/company/controller/algortims/Alg1.java
+++ src/main/java/com/company/controller/algortims/Alg1.java
@@ -3,13 +3,14 @@ package com.company.controller.algortims;
 public class Alg1 {
     public static void main(String[] args) {
         int[] arr = {23, 45, 78, 2, 34, 12, 5, 21, 14, 19, 42, 66, 5};
-        for (int print : arr) {
-            System.out.print(print + ", ");
-        }
+//        for (int print : arr) {
+//            System.out.print(print + ", ");
+//        }
 
-        int minArr = minNumforArr(arr);
+        int minArr = minNumberArr(arr);
         System.out.println("min : " + minArr);
-
+        int maxArr = maxNumforArr(arr);
+        System.out.println("max : " + maxArr);
     }
 
     public static int minNumberArr(int[] arr) {
@@ -22,4 +23,13 @@ public class Alg1 {
         return min;
     }
 
+    public static int maxNumforArr(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
 }
