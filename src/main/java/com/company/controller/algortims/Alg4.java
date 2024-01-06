2535647a13d580ab4778886e853eedc360d58041
RashadHamidli <mr_rashad@email.com>
2024-01-06 18:03:49 +0400

    add

--- src/main/java/com/company/controller/algortims/Alg4.java
+++ src/main/java/com/company/controller/algortims/Alg4.java
@@ -2,22 +2,17 @@ package com.company.controller.algortims;
 
 public class Alg4 {
     public static void main(String[] args) {
-        int reqem = 0;
-        String word = "iyirmi bes";
-        String[] s = word.split(" ");
-        int[] a = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-        String[] b = {"sifir", "bir", "iki", "uc", "dord", "bes", "alti", "yeddi", "sekkiz", "doqquz", "on", "iyirmi", "otuz", "qirx", "elli", "altimis", "yetmis", "seksen", "doxsan", "yuz"};
-
-        for (int i = 0; i < s.length; i++) {
-            for (int j = 0; j < b.length; j++) {
-                if (s[i].equals(b[j])) {
-                    reqem += a[j];
-                    break;
-                }
-            }
+        Integer enter = 125;
+        int a = 0;
+        while (enter>0){
+            a+=enter%10;
+            enter/=10;
         }
+        System.out.println(a);
+        String[] soz = {"sifir", " bir", "iki", "uc", "dord", "bes", "alti", "yeddi", "sekkiz", "doqquz", "on", "iyirmi", "otuz", "qirx", "elli", "altimis", "yetmis", "seksen", "doxsan", "yuz"};
+        int[] reqem = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 
-        System.out.println(reqem);
+
     }
 }
 
