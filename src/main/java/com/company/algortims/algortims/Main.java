58407e43cd86c5de29f183ed5a9f88cf6c016f43
RashadHamidli <mr_rashad@email.com>
2024-01-14 12:57:28 +0400

    add'

--- src/main/java/com/company/algortims/algortims/Main.java
+++ src/main/java/com/company/algortims/algortims/Main.java
@@ -1,7 +1,26 @@
 package com.company.algortims.algortims;
 
+import jdk.jfr.StackTrace;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
+        List<ArrayList> myVar=new ArrayList<>();
+        int size = myVar.size();
+        System.out.println(size);
+        String str1 = "Hello";
+        String str2 = new String("Hello");
+        String str3 = "Hello";
+        System.out.println(str1.equals(str2));
+        try {
+            int result = 5 / 0;
+        } catch (ArithmeticException e) {
+            String localizedMessage = e.getLocalizedMessage();
+            System.out.println("LocalizedMessage: " + localizedMessage);
+        }
         Calculator calculator = new Calculator() {
             @Override
             public double calc(double a, double b) {
@@ -10,6 +29,10 @@ public class Main {
         };
         double calc = calculator.calc(5, 3);
         System.out.println(calc);
+
+
+        Integer reduce = Stream.of(3, 4, 5).reduce(0, Integer::sum);
+        System.out.println(reduce);
 
     }
 }
