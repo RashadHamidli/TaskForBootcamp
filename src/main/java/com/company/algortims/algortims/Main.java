3bc1c72376044e5be6dcc4f3b806c8114327be34
RashadHamidli <mr_rashad@email.com>
2024-01-15 11:27:08 +0400

    add

--- src/main/java/com/company/algortims/algortims/Main.java
+++ src/main/java/com/company/algortims/algortims/Main.java
@@ -1,38 +1,15 @@
 package com.company.algortims.algortims;
 
-import jdk.jfr.StackTrace;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<ArrayList> myVar=new ArrayList<>();
-        int size = myVar.size();
-        System.out.println(size);
-        String str1 = "Hello";
-        String str2 = new String("Hello");
-        String str3 = "Hello";
-        System.out.println(str1.equals(str2));
-        try {
-            int result = 5 / 0;
-        } catch (ArithmeticException e) {
-            String localizedMessage = e.getLocalizedMessage();
-            System.out.println("LocalizedMessage: " + localizedMessage);
-        }
-        Calculator calculator = new Calculator() {
-            @Override
-            public double calc(double a, double b) {
-                return a + b;
-            }
-        };
-        double calc = calculator.calc(5, 3);
-        System.out.println(calc);
-
-
-        Integer reduce = Stream.of(3, 4, 5).reduce(0, Integer::sum);
-        System.out.println(reduce);
+        List<String> list = new ArrayList();
+        list.add("bir");
+        list.stream().forEach(System.out::println);
+        String s = (String) list.get(0);
+        System.out.println(s);
 
     }
 }
---
+++ src/main/java/com/company/controller/Main.java
@@ -0,0 +1,15 @@
+package com.company.controller;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        CreateCode createCode = new CreateCode();
+        String ayrac = "---------------";
+        System.out.println(ayrac + "\n<Oluşturulan Lisans Anahtarları>\n" + ayrac);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("-> Oluşturulan " + (i + 1) + ". Lisans Anahtarı: " + createCode.newCode() + "\n" + ayrac);
+        }
+    }
+}
