f423fd0d40656cecd2dd0a14d5f15db5e9ed3af9
RashadHamidli <mr_rashad@email.com>
2024-01-27 10:40:19 +0400

    add

---
+++ src/main/java/com/company/entity/Eskiz.java
@@ -0,0 +1,16 @@
+package com.company.entity;
+
+
+import java.util.Date;
+
+public class Eskiz {
+    private int id;
+    private String address;
+    private int saygac;
+    private int coefficient;
+    private Date date;
+    private Equipment equipment;
+    private Employee employee;
+
+
+}
