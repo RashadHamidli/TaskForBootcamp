399140664f6679732237173b38cd1ebf10c82806
RashadHamidli <mr_rashad@email.com>
2024-01-27 11:40:29 +0400

    add

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -2,6 +2,25 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        Thread threadA = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println(i + " A" + Thread.currentThread());
+            }
+        });
+        Thread threadB = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println(i + " B" + Thread.currentThread());
+            }
+        });
+
+        Thread threadC=new Thread(() -> {
+            for (int i = 0; i <5; i++) {
+                System.out.println(i + " C" + Thread.currentThread());
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+        threadC.start();
     }
 }
