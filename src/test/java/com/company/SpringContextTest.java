9dabf64dda62ffd4f60ea1963408d5b3ce544866
RashadHamidli <mr_rashad@email.com>
2024-02-07 12:18:42 +0400

    add

--- src/test/java/com/company/SpringContextTest.java
+++ src/test/java/com/company/SpringContextTest.java
@@ -1,10 +1,8 @@
 package com.company;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringContextTest {
 
