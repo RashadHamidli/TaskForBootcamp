060977007fb166a5503697902cc5fd3421a7edd5
RashadHamidli <mr_rashad@email.com>
2024-02-07 13:41:42 +0400

    add

--- src/main/java/com/company/security/SecurityConfig.java
+++ src/main/java/com/company/security/SecurityConfig.java
@@ -1,37 +1,28 @@
-//package com.company.security;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
-//
-//import static org.springframework.security.config.Customizer.withDefaults;
-//import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-////    @Bean
-////    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-////        http.authorizeHttpRequests(expressionInterceptUrlRegistry -> expressionInterceptUrlRegistry
-////                                .anyRequest()
-////                                .permitAll())
-////                .csrf(AbstractHttpConfigurer::disable);
-////        return http.build();
-////    }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests((authz) -> authz
-//                        .anyRequest().permitAll()
-//                )
-//                .csrf(AbstractHttpConfigurer::disable);
-//        return http.build();
-//    }
-//}
+package com.company.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(expressionInterceptUrlRegistry -> expressionInterceptUrlRegistry
+                                .anyRequest()
+                                .permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+
+}
