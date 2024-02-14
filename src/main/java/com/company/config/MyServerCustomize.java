07843fd02c4069527fd26ae314350d90f31471c4
RashadHamidli <mr_rashad@email.com>
2024-02-14 18:57:57 +0400

    add

---
+++ src/main/java/com/company/config/MyServerCustomize.java
@@ -0,0 +1,11 @@
+package com.company.config;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+
+public class MyServerCustomize implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        factory.setPort(8081);
+    }
+}
--- src/main/java/com/company/entity/User.java
+++ src/main/java/com/company/entity/User.java
@@ -1,14 +1,19 @@
 package com.company.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
 @Entity
 @Table(name = "users")
+@Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "name cannot be null")
     private String name;
+    @NotBlank(message = "surname cannot be null")
     private String surname;
 
 }
