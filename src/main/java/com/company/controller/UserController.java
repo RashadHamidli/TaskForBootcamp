be280bbd4d2a2d710ac31d378014078ad8ac4bbe
RashadHamidli <mr_rashad@email.com>
2024-02-26 21:02:11 +0400

    add

--- src/main/java/com/company/controller/UserController.java
+++ src/main/java/com/company/controller/UserController.java
@@ -1,13 +1,35 @@
 package com.company.controller;
 
+import com.company.entity.User;
 import com.company.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @GetMapping("/all")
+    @ResponseStatus(HttpStatus.OK)
+    private List<User> getAllUser() {
+        return userService.getAllUser();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    private User getUserById(@PathVariable long id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping("/{email}")
+    @ResponseStatus(HttpStatus.OK)
+    private User getUserByEmail(@PathVariable String email) {
+        return userService.getUserByEmail(email);
+    }
+
 }
--- src/main/java/com/company/service/UserService.java
+++ src/main/java/com/company/service/UserService.java
@@ -1,11 +1,26 @@
 package com.company.service;
 
+import com.company.entity.User;
 import com.company.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    public List<User> getAllUser() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(long id) {
+        return userRepository.findById(id).orElseThrow();
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow();
+    }
 }
