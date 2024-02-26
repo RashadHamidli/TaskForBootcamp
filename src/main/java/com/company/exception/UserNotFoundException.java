0d35646ea51a859393dfb8a8d04f3fb6ba9d91b6
RashadHamidli <mr_rashad@email.com>
2024-02-26 19:57:42 +0400

    add

--- src/main/java/com/company/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.company.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
