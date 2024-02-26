d3c942a1f49f58bd77dff33312f8fc36e6022c25
RashadHamidli <mr_rashad@email.com>
2024-02-26 21:18:22 +0400

    add

--- src/main/java/com/company/controller/UserController.java
+++ src/main/java/com/company/controller/UserController.java
@@ -2,6 +2,8 @@ package com.company.controller;
 
 import com.company.entity.User;
 import com.company.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,23 +13,24 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
+@Api(tags = "User API")
 public class UserController {
     private final UserService userService;
 
     @GetMapping("/all")
-    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation("Get all users")
     private List<User> getAllUser() {
         return userService.getAllUser();
     }
 
     @GetMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation("Get user by ID")
     private User getUserById(@PathVariable long id) {
         return userService.getUserById(id);
     }
 
     @GetMapping("/{email}")
-    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation("Get user by email")
     private User getUserByEmail(@PathVariable String email) {
         return userService.getUserByEmail(email);
     }
