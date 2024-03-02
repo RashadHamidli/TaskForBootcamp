16ca6b68883bedbbf69e4605d027267004f2adf0
RashadHamidli <mr_rashad@email.com>
2024-03-02 11:52:34 +0400

    add

--- src/main/java/com/company/controllers/LoginRestController.java
+++ src/main/java/com/company/controllers/LoginRestController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,7 @@ public class LoginRestController {
         UsernamePasswordAuthenticationToken unauthenticated =
                 UsernamePasswordAuthenticationToken.unauthenticated(loginRequest.username(), loginRequest.password());
         Authentication authenticate = authenticationManager.authenticate(unauthenticated);
+        SecurityContextHolder.getContext().setAuthentication(authenticate);
         return ResponseEntity.ok(authenticate);
     }
 }
