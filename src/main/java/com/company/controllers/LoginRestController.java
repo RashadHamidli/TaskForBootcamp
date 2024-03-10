4ac1995dbedad731538bd1612d3ac0c61f9cf790
RashadHamidli <mr_rashad@email.com>
2024-03-10 09:41:50 +0400

    add

--- src/main/java/com/company/controllers/LoginRestController.java
+++ src/main/java/com/company/controllers/LoginRestController.java
@@ -5,7 +5,7 @@ import com.company.dto.LoginRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -31,11 +31,12 @@ public class LoginRestController {
 //    }
 
     @PostMapping
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
-        Authentication authentication = new TestingAuthenticationToken("admin", "admin", "ROLE_ADMIN");
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String redirectUrl = "http://localhost:8080/api/user/all";
-        return ResponseEntity.ok(redirectUrl);
+    public ResponseEntity<Authentication> login(@RequestBody LoginRequest loginRequest) {
+        UsernamePasswordAuthenticationToken unauthenticated =
+                UsernamePasswordAuthenticationToken.unauthenticated(loginRequest.username(), loginRequest.password());
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(unauthenticated);
+        return ResponseEntity.ok(context.getAuthentication());
     }
 }
 
